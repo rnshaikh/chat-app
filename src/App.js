e9d8ad5ead91c17d8b655ebf2c6610e28a12848f
Rizwan Shaikh <rshaikh@ces-ltd.com>
2021-03-21 00:15:50 +0530

    Added chat-app in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	if(! localStorage.getItem('username')) return <LoginForm></LoginForm>
+	const username = localStorage.getItem('username')
+	const password = localStorage.getItem('password')
+	return (
+		<ChatEngine
+      		height= '100vh'
+			projectID='app-id'
+			userName={username}
+      		userSecret={password}
+      		renderChatFeed = {(chatAppProps)=> <ChatFeed {...chatAppProps} /> }
+		/>
+	);
 }
 
 export default App;
